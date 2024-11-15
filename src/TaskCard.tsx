3656551ac3ffcbea193303f5b64c1ad0123f343b
RomaSamkov <Romansamkov.rs@gmail.com>
2024-11-15 16:59:46 +0200

    Update TaskCard.tsx

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -1,3 +1,4 @@
+import { useDraggable } from "@dnd-kit/core";
 import { Task } from "./types";
 
 type TaskCardProps = {
@@ -5,13 +6,20 @@ type TaskCardProps = {
 };
 
 export default function TaskCard({ task }: TaskCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task.id,
+  });
+
+  const style = transform
+    ? { transform: `translate(${transform.x}px, ${transform.y}px)` }
+    : undefined;
   return (
     <div
-      //   ref={setNodeRef}
-      //   {...listeners}
-      //   {...attributes}
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
       className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md"
-      //   style={style}
+      style={style}
     >
       <h3 className="font-medium text-neutral-100">{task.title}</h3>
       <p className="mt-2 text-sm text-neutral-400">{task.description}</p>
